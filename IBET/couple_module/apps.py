c2d655c03ade38a22678eb78be4f38ee8d76cafb
ruban1613 <rubanchakravarthy1@gmail.com>
2025-10-16 23:42:53 +0530

    ruban 23:42

--- IBET/couple_module/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoupleModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'couple_module'
-
-    def ready(self):
-        import couple_module.translation  # noqa
