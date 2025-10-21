3d9c3e6e8c5a14e813b95fcb0377768fb8d1729a
ruban1613 <rubanchakravarthy1@gmail.com>
2025-10-21 11:34:05 +0530

    Sync with remote and clean up retiree module files

--- IBET/retiree_module/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RetireeModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'retiree_module'
-
-    def ready(self):
-        import retiree_module.translation  # noqa
